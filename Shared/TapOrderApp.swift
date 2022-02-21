bb4246bd28ef7ba95eba3ce38d0418e5a0925bb8
xu Sun <ssnfee@gmail.com>
2022-02-21 09:47:01 +0800

    Initial Commit

---
+++ Shared/TapOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapOrderApp.swift
+//  Shared
+//
+//  Created by solo on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct TapOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
