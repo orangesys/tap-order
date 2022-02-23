f60ab947ba61614ad2bb86fbc4e48735d253af73
sseen <ssnfee@gmail.com>
2022-02-23 08:32:32 +0800

    add firebase

--- Shared/TapOrderApp.swift
+++ Shared/TapOrderApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TapOrderApp: App {
+    
+    init() {
+      FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
